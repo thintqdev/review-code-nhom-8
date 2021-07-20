77601ee1f7b8c0f22570952e9bdce5630719316e
truongaxin123 <truongaxin123@gmail.com>
2021-07-20 13:55:48 +0700

    add file

--- Xin.cs
+++ Xin.cs
@@ -1,6 +1,18 @@
 using System;
 
-public class Xin {
-    public string Name;
-    public string Address; 
+public class Xin
+{
+
+    private string _name;
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public string Address;
+
+    public Xin(string name)
+    {
+        _name = name;
+    }
 }
