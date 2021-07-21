8f8280016cd2116d75c02a64fb2c13d44f31320f
HIEU <honguyenxuanhieu@MacBook-Pro-cua-Xuan-Hieu.local>
2021-07-21 15:00:04 +0700

    chu cai cua moi tu noi theo sau phai ghi hoa trong comment

--- Form1.cs
+++ Form1.cs
@@ -23,8 +23,8 @@ namespace Calculator_Basic
             So1 = int.Parse(textBox1.Text.ToString());
             So2 = int.Parse(textBox2.Text.ToString());
             double KETQUA;
-            KETQUA = So1 / So2;
-            textBox3.Text = KETQUA.ToString();
+            KETQUA = So1 / So2; 
+            textBox3.Text = KETQUA.ToString();// chi ghi hoa chu cai dau cho KETQUA
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,9 +32,9 @@ namespace Calculator_Basic
             int So1, So2;
             So1 = int.Parse(textBox1.Text.ToString());
             So2 = int.Parse(textBox2.Text.ToString());
-            double KETQUA;
-            KETQUA = So1 + So2;
-            textBox3.Text = KETQUA.ToString();
+            double KETQUA; // chi ghi hoa chu cai dau cho KETQUA
+            KETQUA = So1 + So2;// chi ghi hoa chu cai dau cho KETQUA
+            textBox3.Text = KETQUA.ToString(); // chi ghi hoa chu cai dau cho KETQUA
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -42,8 +42,8 @@ namespace Calculator_Basic
             int So1, So2;
             So1 = int.Parse(textBox1.Text.ToString());
             So2 = int.Parse(textBox2.Text.ToString());
-            double KETQUA;
-            KETQUA = So1 - So2;
+            double KETQUA;// chi ghi hoa chu cai dau cho KETQUA
+            KETQUA = So1 - So2; // chi ghi hoa chu cai dau cho KETQUA
             textBox3.Text = KETQUA.ToString();
         }
 
@@ -52,9 +52,9 @@ namespace Calculator_Basic
             int So1, So2;
             So1 = int.Parse(textBox1.Text.ToString());
             So2 = int.Parse(textBox2.Text.ToString());
-            double KETQUA;
+            double KETQUA;// chi ghi hoa chu cai dau cho KETQUA
             KETQUA = So1 * So2;
-            textBox3.Text = KETQUA.ToString();
+            textBox3.Text = KETQUA.ToString();// chi ghi hoa chu cai dau cho KETQUA
         }
     }
 }
